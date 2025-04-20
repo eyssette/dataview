5716f7ce15b483eddc6f6a6070bc2642034f591d
Cédric Eyssette <cedric.eyssette@ac-lyon.fr>
2025-04-20 16:05:15 +0200

    Updates

---
+++ _app/immutable/chunks/2-4996ac81.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-63752ae5.js";export{t as component};
--- _app/immutable/chunks/2-bcb827c0.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-302019c7.js";export{t as component};
